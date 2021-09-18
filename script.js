216d646ccbee99d3d66267ed460f00206a994c29
tuanta <tuanta04101996@gmail.com>
2021-09-18 21:01:40 +0700

    WIP: UI

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-
+$(window).on('load', function(){
     $("input[name='layout-case']").change(function() {
         const id = $("input[type='radio'][name='layout-case']:checked").attr("id");
 
@@ -82,33 +81,36 @@ $(document).ready(function(){
         const id = $("input[type='radio'][name='plate-material']:checked").attr("id");
 
         if (id.includes("alu")) {
-            $(".plate-color").show();
+            $(".plate-color").css("visibility", "visible");
             $(".plate-alu-color").show();
             $(".plate-pc-color").hide();
             $(".plate-pom-color").hide();
         } else if (id.includes("pc")) {
-            $(".plate-color").show();
+            $(".plate-color").css("visibility", "visible");
             $(".plate-alu-color").hide();
             $(".plate-pc-color").show();
             $(".plate-pom-color").hide();
         } else if (id.includes("pom")) {
-            $(".plate-color").show();
+            $(".plate-color").css("visibility", "visible");
             $(".plate-alu-color").hide();
             $(".plate-pc-color").hide();
             $(".plate-pom-color").show();
         } else {
-            $(".plate-color").hide();
+            // $(".plate-color").hide();
+            $(".plate-color").css("visibility", "hidden");
         }
     });
 
-    $("input[name='layout-case'][id='case-alu-wk']").prop("checked", true);
-    $("input[name='case-color'][id='case-alu-eblack']").prop("checked", true);
-    $("input[name='badge-material'][id='badge-alu']").prop("checked", true);
-    $("input[name='badge-color'][id='badge-alu-ewhite']").prop("checked", true);
-    $("input[name='weight-style'][id='weight-hybrid']").prop("checked", true);
-    $("input[name='weight-material'][id='weight-alu']").prop("checked", true);
-    $("input[name='weight-color'][id='weight-alu-ewhite']").prop("checked", true);
-    $("input[name='subweight-color'][id='subweight-alu-eblack']").prop("checked", true);
-    $("input[name='plate-material'][id='plate-alu']").prop("checked", true);
-    $("input[name='plate-color'][id='plate-alu-ewhite']").prop("checked", true);
+    $("input[name='layout-case'][id='case-alu-wk']").prop("checked", true).change();
+    $("input[name='case-color'][id='case-alu-eblack']").prop("checked", true).change();
+    $("input[name='badge-material'][id='badge-alu']").prop("checked", true).change();
+    $("input[name='badge-color'][id='badge-alu-ewhite']").prop("checked", true).change();
+    $("input[name='weight-style'][id='weight-hybrid']").prop("checked", true).change();
+    $("input[name='weight-material'][id='weight-alu']").prop("checked", true).change();
+    $("input[name='weight-color'][id='weight-alu-ewhite']").prop("checked", true).change();
+    $("input[name='subweight-color'][id='subweight-alu-eblack']").prop("checked", true).change();
+    $("input[name='plate-material'][id='plate-alu']").prop("checked", true).change();
+    $("input[name='plate-color'][id='plate-alu-ewhite']").prop("checked", true).change();
+
+    console.log($(".plate-color").height());
 });
