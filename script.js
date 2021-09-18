975b03fcaef5c0b814be90efeaf0d2c3f841b000
tuanta <tuanta04101996@gmail.com>
2021-09-19 00:49:39 +0700

    Add animation to show and hide plate and plate item

--- script.js
+++ script.js
@@ -1,103 +1,124 @@
-$(window).on('load', function(){
-    $("input[name='layout-case']").change(function() {
+$(window).on('load', function () {
+    $("input[name='layout-case']").change(function () {
         const id = $("input[type='radio'][name='layout-case']:checked").attr("id");
 
         if (id.startsWith("case-alu")) {
             $(".case-color").show();
-            $(".case-alu-color").show();
-            $(".case-pc-color").hide();
+            $(".case-color").animate({ marginLeft: '0px' }, "fast");
+            $(".case-alu-color").slideUp("fast", function () {
+                $(".case-pc-color").slideDown();
+            });
         } else if (id.startsWith("case-pc")) {
             $(".case-color").show();
-            $(".case-alu-color").hide();
-            $(".case-pc-color").show();
+            $(".case-color").animate({ marginLeft: '0px' }, "fast");
+            $(".case-pc-color").slideUp("fast", function () {
+                $(".case-alu-color").slideDown();
+            });
         } else {
-            $(".case-color").hide();
+            $(".case-color").animate({ marginLeft: '-2000px' }, function () { $(".case-color").fadeOut("fast"); });
         }
-        
+
         if (id.includes("crane")) {
-            $(".badge-regular").hide();
-            $(".badge-crane").hide();
-            $(".badge-flower").show();
+            $(".badge-crane").slideUp("fast", function () {
+                $(".badge-flower").slideDown();
+                $(".badge-crane").slideDown();
+            });
         } else if (id.includes("flower")) {
-            $(".badge-regular").hide();
-            $(".badge-crane").hide();
-            $(".badge-flower").show();
+            $(".badge-flower").slideUp("fast", function () {
+                $(".badge-regular").slideDown();
+                $(".badge-crane").slideDown();
+            });
         } else {
-            $(".badge-regular").show();
-            $(".badge-crane").hide();
-            $(".badge-flower").hide();
+            $(".badge-regular").slideUp("fast", function () {
+                $(".badge-crane").slideDown();
+                $(".badge-flower").slideDown();
+            });
         }
     });
 
-    $("input[name='badge-material']").change(function() {
+    $("input[name='badge-material']").change(function () {
         const id = $("input[type='radio'][name='badge-material']:checked").attr("id");
 
         if (id.includes("alu")) {
+            $(".badge-alu-color").slideUp("fast", function () {
+                $(".badge-pc-color").slideDown();
+            });
+
             $(".badge-color").show();
-            $(".badge-alu-color").show();
-            $(".badge-pc-color").hide();
+            $(".badge-color").animate({ marginLeft: '0px' }, "fast");
         } else if (id.includes("pc")) {
+            $(".badge-pc-color").slideUp("fast", function () {
+                $(".badge-alu-color").slideDown();
+            });
+
             $(".badge-color").show();
-            $(".badge-alu-color").hide();
-            $(".badge-pc-color").show();
+            $(".badge-color").animate({ marginLeft: '0px' }, "fast");
         } else {
-            $(".badge-color").hide();
+            $(".badge-color").animate({ marginLeft: "-2000px" }, function () { $(".badge-color").fadeOut("fast"); });
         }
     });
 
-    $("input[name='weight-style']").change(function() {
+    $("input[name='weight-style']").change(function () {
         const id = $("input[type='radio'][name='weight-style']:checked").attr("id");
 
         if (id.includes("regular")) {
-            $(".subweight-material").hide();
-            $(".subweight-color").hide();
+            $(".subweight-material").animate({ marginLeft: "-2000px" }, function () { $(".subweight-material").fadeOut("fast"); });
+            $(".subweight-color").slideDown();
         } else {
             $(".subweight-material").show();
-            $(".subweight-color").show();
+            $(".subweight-material").animate({ marginLeft: '0px' }, "fast");
+            $(".subweight-color").slideUp("fast");
         }
     });
 
-    $("input[name='weight-material'").change(function() {
+    $("input[name='weight-material'").change(function () {
         const id = $("input[type='radio'][name='weight-material']:checked").attr("id");
 
         if (id.includes("alu")) {
             $(".weight-color").show();
+            $(".weight-color").animate({ marginLeft: '0px' }, "fast");
         } else {
-            $(".weight-color").hide();
+            $(".weight-color").animate({ marginLeft: "-2000px" }, function () { $(".weight-color").fadeOut("fast"); });
         }
     });
 
-    $("input[name='subweight-material'").change(function() {
+    $("input[name='subweight-material'").change(function () {
         const id = $("input[type='radio'][name='subweight-material']:checked").attr("id");
 
         if (id.includes("alu")) {
             $(".subweight-color").show();
+            $(".subweight-color").animate({ marginLeft: '0px' }, "fast");
         } else {
-            $(".subweight-color").hide();
+            $(".subweight-color").animate({ marginLeft: "-2000px" }, function () { $(".subweight-color").fadeOut("fast"); });
         }
     });
 
-    $("input[name='plate-material'").change(function() {
+    $("input[name='plate-material'").change(function () {
         const id = $("input[type='radio'][name='plate-material']:checked").attr("id");
 
         if (id.includes("alu")) {
-            $(".plate-color").css("visibility", "visible");
-            $(".plate-alu-color").show();
-            $(".plate-pc-color").hide();
-            $(".plate-pom-color").hide();
+            $(".plate-color").show();
+            $(".plate-color").animate({ marginLeft: '0px' }, "fast");
+            $(".plate-alu-color").slideUp("fast", function () {
+                $(".plate-pc-color").slideDown();
+                $(".plate-pom-color").slideDown();
+            });
         } else if (id.includes("pc")) {
-            $(".plate-color").css("visibility", "visible");
-            $(".plate-alu-color").hide();
-            $(".plate-pc-color").show();
-            $(".plate-pom-color").hide();
+            $(".plate-color").show();
+            $(".plate-color").animate({ marginLeft: '0px' }, "fast");
+            $(".plate-pc-color").slideUp("fast", function () {
+                $(".plate-alu-color").slideDown();
+                $(".plate-pom-color").slideDown();
+            });
         } else if (id.includes("pom")) {
-            $(".plate-color").css("visibility", "visible");
-            $(".plate-alu-color").hide();
-            $(".plate-pc-color").hide();
-            $(".plate-pom-color").show();
+            $(".plate-color").show();
+            $(".plate-color").animate({ marginLeft: '0px' }, "fast");
+            $(".plate-pom-color").slideUp("fast", function () {
+                $(".plate-alu-color").slideDown();
+                $(".plate-pc-color").slideDown();
+            });
         } else {
-            // $(".plate-color").hide();
-            $(".plate-color").css("visibility", "hidden");
+            $(".plate-color").animate({ marginLeft: "-2000px" }, function () { $(".plate-color").fadeOut("fast"); });
         }
     });
 
@@ -111,6 +132,4 @@ $(window).on('load', function(){
     $("input[name='subweight-color'][id='subweight-alu-eblack']").prop("checked", true).change();
     $("input[name='plate-material'][id='plate-alu']").prop("checked", true).change();
     $("input[name='plate-color'][id='plate-alu-ewhite']").prop("checked", true).change();
-
-    console.log($(".plate-color").height());
 });
